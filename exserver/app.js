4ffa5951ceaec75e9ce4831f6f91f0873f93eb82
Yuting Chou <ytingchou@gmail.com>
2017-01-06 11:56:49 +1100

    Created a static web server

--- exserver/app.js
+++ exserver/app.js
@@ -1,14 +1,10 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-});
-
-app.get('/about', function(req, res) {
-  res.send('<h1>About Us</h1>');
-});
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function() {
   console.log('server started on port 3000');
