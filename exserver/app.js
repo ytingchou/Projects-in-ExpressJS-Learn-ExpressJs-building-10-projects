9e31bc171dd2ccfdc09dc22acc6962b80f682f40
Yuting Chou <ytingchou@gmail.com>
2017-01-06 12:38:32 +1100

    Added more routes.

--- exserver/app.js
+++ exserver/app.js
@@ -6,6 +6,39 @@ var app = express();
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/people', function(req, res) {
+  var people = [
+    {
+      first_name: "John",
+      last_name: "Doe",
+      age: 34,
+      gender: "male"
+    },
+    {
+      first_name: "Tom",
+      last_name: "Jackson",
+      age: 27,
+      gender: "male"
+    },
+    {
+      first_name: "Tracy",
+      last_name: "Smith",
+      age: 30,
+      gender: "female"
+    }
+  ];
+
+  res.json(people);
+});
+
+app.get('/download', function(req, res) {
+  res.download(path.join(__dirname, 'downloads/pdf-sample.pdf'));
+});
+
+app.get('/about', function(req, res) {
+  res.redirect('about.html');
+});
+
 app.listen(3000, function() {
   console.log('server started on port 3000');
 });
