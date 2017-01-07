9fe9e6132a82e7d724fb363aebcf3e202911c349
Yuting Chou <ytingchou@gmail.com>
2017-01-07 18:13:08 +1100

    Added services page.

--- pcrepair/app.js
+++ pcrepair/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
+var services = require('./routes/services');
 var users = require('./routes/users');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
+app.use('/services', services);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
